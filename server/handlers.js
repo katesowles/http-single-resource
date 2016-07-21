f74bdf64b48f6dbaee03f76419c6d408cb3f0afe
Kate Sowles <katesowles@gmail.com>
2016-07-21 09:38:56 -0700

    adding mocha

--- server/handlers.js
+++ server/handlers.js
@@ -1,39 +1,39 @@
-function one (response) {
-  console.log('request handler "one" was called.');
-
-  let body = '<html>' +
-    '<head>' +
-      '<meta charset="utf-8">' +
-      '<title>Page One</title>' +
-    '</head>' +
-    '<body>' +
-      '<h1>This is Page One</h1>' +
-      '<a href="/two"><h3>Click here to go to Page Two</h3></a>' +
-    '</body>' +
-  '</html>';
-
-  response.writeHead(200, {'Content-Type' : 'text/html'});
-  response.write(body);
-  response.end();
-}
-
-function two (response) {
-  console.log('request handler "two" was called.');
-
-  let body = '<html>' +
-      '<head>' +
-        '<meta charset="utf-8">' +
-        '<title>Page Two</title>' +
-      '</head>' +
-      '<body>' +
-        '<h1>This is Page Two</h1>' +
-        '<a href="/one"><h3>Click here to go to Page One</h3></a>' +
-      '</body>' +
-    '</html>';
-  response.writeHead(200, {'Content-Type' : 'text/html'});
-  response.write(body);
-  response.end();
-}
-
-exports.one = one;
-exports.two = two;
+// function one (response) {
+//   console.log('request handler "one" was called.');
+//
+//   let body = '<html>' +
+//     '<head>' +
+//       '<meta charset="utf-8">' +
+//       '<title>Page One</title>' +
+//     '</head>' +
+//     '<body>' +
+//       '<h1>This is Page One</h1>' +
+//       '<a href="/two"><h3>Click here to go to Page Two</h3></a>' +
+//     '</body>' +
+//   '</html>';
+//
+//   response.writeHead(200, {'Content-Type' : 'text/html'});
+//   response.write(body);
+//   response.end();
+// }
+//
+// function two (response) {
+//   console.log('request handler "two" was called.');
+//
+//   let body = '<html>' +
+//       '<head>' +
+//         '<meta charset="utf-8">' +
+//         '<title>Page Two</title>' +
+//       '</head>' +
+//       '<body>' +
+//         '<h1>This is Page Two</h1>' +
+//         '<a href="/one"><h3>Click here to go to Page One</h3></a>' +
+//       '</body>' +
+//     '</html>';
+//   response.writeHead(200, {'Content-Type' : 'text/html'});
+//   response.write(body);
+//   response.end();
+// }
+//
+// exports.one = one;
+// exports.two = two;
